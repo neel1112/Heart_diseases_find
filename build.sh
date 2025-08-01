ceed6037a393461da80f1e886c18729db6149521
Merja Neel. <neelmerja@gmail.com>
2025-08-01 18:32:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install setuptools first to ensure build dependencies are available
+pip install --upgrade pip setuptools wheel
+
+# Install requirements
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate 
+
+# Run migrations
+python manage.py migrate
